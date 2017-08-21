fc30c0681d4790bd7a536f0b28258c9ffc5e0d4e
F4rr3LL <shadow_2pac@mail.ru>
2017-08-21 11:31:02 +0300

    удален слайдер

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 jQuery(document).ready(function($) {
-    $('.carouselMain').carousel();
-
     $(".scroll").click(function(event){		
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
